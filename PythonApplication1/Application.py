33794953ae9db582b3c97719125bb1566867f16a
Lucas Martins <lgatts@gmail.com>
2017-11-13 21:16:13 -0200

    Desenhando da tela

--- PythonApplication1/Application.py
+++ PythonApplication1/Application.py
@@ -18,7 +18,8 @@ class Application(tk.Frame):
         #canvas onde é mostrada a imagem e onde se pode desenhar
         self.canvas = Canvas(self.master)        
         self.canvas.pack(expand = YES, fill = BOTH)
-        self.canvas.bind( "<B1-Motion>", self.paint )
+        #chamada da função de desenhar na tela
+        self.canvas.bind("<B1-Motion>", self.paint)
 
     def fileChooserButtonClick(self):
         filename = filedialog.askopenfilename(initialdir = "./images",title = "Select Image",filetypes = (("Valid formats","*.png;*gif"),("all files","*.*")))
@@ -29,13 +30,13 @@ class Application(tk.Frame):
         self.canvas["width"] = self.img.width()
         self.canvas["height"] = self.img.height()
         self.canvas.create_image(0,0,anchor=NW,image=self.img)        
-        
-    def paint(self, event ):
-        python_green="#604a42"
-        #python_green = "#476042"
-        x1, y1 = ( event.x - 1 ), ( event.y - 1 )
-        x2, y2 = ( event.x + 1 ), ( event.y + 1 )
-        self.canvas.create_oval( x1, y1, x2, y2, fill = python_green )
+    
+    #função que le o evento do mouse e desenha na tela
+    def paint(self, event):        
+        python_green = "#476042"
+        x1, y1 = (event.x - 1), (event.y - 1)
+        x2, y2 = (event.x + 1), (event.y + 1)
+        self.canvas.create_oval(x1, y1, x2, y2, fill = python_green)
 
  
 root = tk.Tk()
